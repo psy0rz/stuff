42e2dfdf213fcc259189603ebe8898f1744f1524
Edwin Eefting <edwin@datux.nl>
2014-01-07 14:48:20 +0100

    cleaner pinconfig

--- rfid/pin_config_frontdoor.h
+++ rfid/pin_config_frontdoor.h
@@ -1,45 +1,28 @@
 #ifndef pin_config_h
 #define pin_config_h
-/*
-Pin outs
 
-description     uno pin    MFRC522 rfid pin  RF24 pin                      
-GND             GND        gnd(orange)       1 (gnd,brown)  
-3v3             VCC        3.3v(brown)       2 (vcc,red) 3(ce,orange)
-SPI MOSI        D11        MOSI(blue)        6 (mosi,blue)
-SPI MISO        D12        MISO(green)       7 (miso,violet)
-SPI SCK         D13        SCK(violet)       5 (sck,green)
-CS RF24         D10                          4 (csn,yellow)
-CS MFRC522      D8   
-RST             D9         RST(red)
-IRQ             D2                           8 (irq,gray)
-lock pin        D6
-manual pin      D7
-led pin         D4
+////////////////////////////// Pin outs
+//                          //
+//arduino pin               // description     MFRC522 rfid pin     RF24 pin                      
+//GND                       // GND             gnd(green)           1 (gnd,brown)  
+//3v3                       // 3v3             3.3v(orange)         2 (vcc,red) 3(ce,orange)
+//11                        // SPI MOSI        MOSI(gray)           6 (mosi,blue)
+//12                        // SPI MISO        MISO(violet)         7 (miso,violet)
+//13                        // SPI SCK         SCK(white)           5 (sck,green)
+#define CS_RF24_PIN 7       // CS RF24                              4 (csn,yellow)
+#define CS_MFRC522_PIN 10   // CS MFRC522      SDA(black)   
+#define RST_PIN 8           // RST             RST(yellow)
+                            // IRQ                                  8 (irq,gray)
+#define RFID_LOCK_PIN 9     // lock pin        
+#define RFID_MANUAL_PIN 6   // manual pin      
+#define RFID_LED_PIN 4      // led pin         
 
-*/
-
-//chip select for RF24 SPI
-#define CS_RF24_PIN 8
-
-//chip select MFRC522 SPI
-#define CS_MFRC522_PIN 8
-
- //reset pin for all SPI devices
-#define RST_PIN 10
-
-#define RFID_LED_PIN 4             //feedback led
 
 
 //lock powersaving stuff
-#define RFID_LOCK_PIN 9            //lock output
 #define RFID_LOCK_DUTY_ON      100    //full on time in ms when locking
 #define RFID_LOCK_DUTY_TOTAL   10000  //total time of pwm duty cycle 
 //the pwm value is stored in config.lock_pwm
-
-#define RFID_MANUAL_PIN 6          //manual open by switch pin
-//#define RFID_125KHZ_PIN 5 //rx pin for 125khz module
-
 
 
 #endif
