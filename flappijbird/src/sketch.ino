d0b503c730745fc4cd0317bd889cc5dfb1b1d115
Edwin Eefting <edwin@datux.nl>
2015-04-11 22:15:31 +0200

    bird physics work

--- flappijbird/src/sketch.ino
+++ flappijbird/src/sketch.ino
@@ -32,44 +32,47 @@ void setup()
 void loop()
 {
   int bird_max=1000;
+  int bird_min=-200;
   int bird_y=bird_max/2;
   int bird_x=3;
   int bird_speed=0;
-  int bird_gravity=7;
-  int bird_jump_speed=70; 
+  int bird_gravity=-7;
+  int bird_jump_speed=60; 
 
-
-  int start_time=millis();
+  unsigned long start_time=millis();
   int frame_time=25;
 
-  bool pressed=false;
+  bool button_state=false;
 
   while(1)
   {
     start_time=millis();
 
+    //gravity, keep accelerating downwards
+    bird_speed=bird_speed+bird_gravity;
 
-    if (!digitalRead(buttonPin) && !pressed)
+    //button changed
+    if ( digitalRead(buttonPin) != button_state)
     {
-        pressed=true;
-        bird_speed=bird_jump_speed;
-    }
-    else
-    {
-        pressed=false;
-        bird_speed=bird_speed-bird_gravity;
+      button_state=digitalRead(buttonPin);
+
+      //its pressed, so jump! 
+      if (!button_state)
+          bird_speed=bird_jump_speed;
     }
 
-    bird_y=bird_y-bird_speed;
+    //change y postion of bird
+    bird_y=bird_y+bird_speed;
 
-    if (bird_y<0)
-      bird_y=0;
+    if (bird_y<bird_min)
+      bird_y=bird_min;
 
     if (bird_y>bird_max)
       bird_y=bird_max;
 
     //downscale birdheight to 8 pixels :P
-    lc.setRow(0, bird_x, (1 << ((bird_y*7) / bird_max)));
+    //( LSB is top pixel )
+    lc.setRow(0, bird_x,  (B10000000 >> ((bird_y*7) / bird_max)) );
 
     //wait for next frame
     while( (millis()-start_time) < frame_time){
