d6205208dd8ccd98988ef835c832e6dda632f429
Edwin Eefting <edwin@datux.nl>
2019-07-18 20:08:55 +0200

    lcd

--- animalchipreader/main.py
+++ animalchipreader/main.py
@@ -3,6 +3,19 @@
 import esp
 esp.osdebug(None)
 
+from machine import I2C, Pin
+from esp8266_i2c_lcd import I2cLcd
+
+
+DEFAULT_I2C_ADDR = 0x27
+i2c = I2C(scl=Pin(25), sda=Pin(26), freq=400000)
+
+# 2x16
+lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
+
+lcd.putstr("searching cat..")
+
+
 ### network stuff
 # import config
 # import network
@@ -35,10 +48,18 @@ prev=0
 count=0
 while True:
 
-    if time.time()!=lastreset:
+    if time.ticks_ms()-lastreset>100:
         r.value(0)
         r.value(1)
-        lastreset=time.time()
+        lastreset=time.ticks_ms()
+        count=count+1
+        print(count)
+
+        lcd.move_to(0,1)
+        if count%2:
+            lcd.putstr("scan")
+        else:
+            lcd.putstr("    ")
 
     # v=p.value()
     # if v!=prev:
@@ -47,22 +68,27 @@ while True:
     #     print(count)
     line=uart.read()
     if line:
-        count=count+1
-        print(count)
         # print(line)
-        card_str=line[1:11].decode()
-        country_str=line[11:15].decode()
 
-        #swap lsb msb:
-        card_str_swapped=list(card_str)
-        card_str_swapped.reverse()
-        card_str_swapped="".join(card_str_swapped)
-        card=int(card_str_swapped,16)
+        try:
+            card_str=line[1:11].decode()
+            country_str=line[11:15].decode()
 
-        country_str_swapped=list(country_str)
-        country_str_swapped.reverse()
-        country_str_swapped="".join(country_str_swapped)
-        country=int(country_str_swapped,16)
+            #swap lsb msb:
+            card_str_swapped=list(card_str)
+            card_str_swapped.reverse()
+            card_str_swapped="".join(card_str_swapped)
+            card=int(card_str_swapped,16)
 
-        #official format:
-        print("{}{:012}".format(country, card))
+            country_str_swapped=list(country_str)
+            country_str_swapped.reverse()
+            country_str_swapped="".join(country_str_swapped)
+            country=int(country_str_swapped,16)
+
+            #official format:
+            tag="{}{:012}".format(country, card)
+            print(tag)
+            lcd.clear()
+            lcd.putstr(tag)
+        except:
+            print("error")
